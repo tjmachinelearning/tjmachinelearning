2432b9be05b795cda068a78560dd61076b317a9f
Mihir Patel <mihir.v.patel7@gmail.com>
2017-09-19 22:47:16 -0400

    update

--- resources/IO_sample.py
+++ resources/IO_sample.py
@@ -1,15 +1,19 @@
 
 #change name of file to load in data from a different file.
 def getData():
-	x = []
-	y = []
-	input = open("data.txt").read().split("\n")
-	for i in input:
-		inputArray = i.split(",")
-		exp = inputArray.pop(len(inputArray)-1)
-		x.append(inputArray)
-		y.append(exp)
-	return x,y
+    x = []
+    y = []
+    input = open("training_xd.csv").read().split("\n")
+    for i in input:
+        inputArray = i.split(",")
+        if(len(inputArray)==10): #num of features + num of labels
+            exp = inputArray.pop(len(inputArray)-1)
+            x.append(inputArray)
+            y.append(exp)
+        else:
+            print(len(inputArray))
+    return x,y
+
 
 
 X, y = getData()
