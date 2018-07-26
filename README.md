bdfcd95d3f51b5826f2afbbbe89e8c8fabc05c44
nikhilsardana <2018nsardana@tjhsst.edu>
2018-07-26 05:25:48 -0400

    major update: add footer, update comp site, change filenames

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Official TJHSST Machine Learning Club Website
 
-[TJ Machine Learning Club Github Organization](https://github.com/tjmachinelearning)
+To view the TJ Machine Learning Club Github Organization, click [here](https://github.com/tjmachinelearning)
--- js/expand.js
+++ js/expand.js
@@ -1,3 +1,11 @@
+// This file was used for the old submit.html (Called Updates)
+//With 3 expandable sections: Competition Instructions, Competition Procedures, and Club Updates
+//And within Competition Instructions section, expandable subsections for each specific competition
+//Inside submit.html, in order for this expand.js to work, the line <script src="js/expand.js"></script> was necessary
+//as well as <section id="______" style="display:none"> ... </section> around expandable content,
+//where ______ can be found in each $('#______').toggle(); line below.
+
+
 $(document).ready(function() {
     $('#comptitle').on('click',function(){
         $('#comp').toggle();
