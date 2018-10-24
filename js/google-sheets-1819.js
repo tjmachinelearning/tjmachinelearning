17980b2618f00b72d6f0d5f6aa40a0e0d84b34db
vbhaip <vbhaip01@gmail.com>
2018-10-24 11:51:17 -0400

    added rand forests rankings

--- js/google-sheets-1819.js
+++ js/google-sheets-1819.js
@@ -16,7 +16,7 @@ var visualization;
 
 function drawVisualization() {
     var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1ueMyiUtdXf3G3g-FRrgPdkI-LftwsoOt_SyKKJ52quU&output=html&usp=sharing');
-    query.setQuery('SELECT A, B, C label A "Name", B "Decision Trees", C "Overall"');
+    query.setQuery('SELECT A, B, C, D label A "Name", B "Decision Trees", C "Random Forests", D "Overall"');
     query.send(handleQueryResponse);
 }
 
