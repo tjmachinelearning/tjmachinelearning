a653223045230fb6cc47004a313232afaf4753df
Aarav-Khanna <50778020+Aarav-Khanna@users.noreply.github.com>
2021-01-20 22:36:55 -0500

    Add rankings

---
+++ js/google-sheets-2021.js
@@ -0,0 +1,38 @@
+/*!
+ *
+ * Google Sheets To HTML v0.9a
+ *
+ * To use, simply replace the "tq?key=" value in the
+ * URL below with your own unique Google document ID
+ *
+ * The Google document's sharing must be set to public
+ *
+ * For privacy purposes, remove the Google document ID
+ * at the end of each year, and comment out query.send()
+ * Replace the full rankings at year's end with a list
+ * of the top three students.
+ */
+
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1OMqxTnPPrGOeif42pnVSdeeQe_8vkUfjhY0RzbAYTWA/edit?usp=sharing');
+    query.setQuery('SELECT A, B, C, D label A "Name", B "Decision Trees/Random Forests", C "Support Vector Machines", D "Total"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
