20dd9efd9268094f921c09b2b835425fd4ff182d
Charlie Wu <cwu159@gmail.com>
2021-03-28 01:03:14 -0400

    Update google-sheets-2021.js

--- js/google-sheets-2021.js
+++ js/google-sheets-2021.js
@@ -20,10 +20,8 @@ var visualization;
 
 function drawVisualization() {
 //     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1BbZ6ddViIMufL0mj3NMDkljT7QlmQYxlZZe26y6L254/edit?usp=sharing');
-//     var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1BbZ6ddViIMufL0mj3NMDkljT7QlmQYxlZZe26y6L254&output=html&usp=sharing');
-//     query.setQuery('SELECT A, B, C, D, E, F label A "Name", B "Decision Trees/Random Forests", C "Support Vector Machines", D "Neural Networks" E "Convolutional Neural Networks", F "Total"');
-    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1e6Fpo3WnBqhIcU1wx4q9IvDdizmHDo98v9aNVWEdJfU&output=html&usp=sharing');
-    query.setQuery('SELECT A, B, C, D, E, F, G, H label A "Name", B "Decision Trees Pset", C "Decision Trees Comp", D "Random Forest Comp", E "SVM Comp", F "Neural Net Comp", G "NN Pset", H "Total"');
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1BbZ6ddViIMufL0mj3NMDkljT7QlmQYxlZZe26y6L254&output=html&usp=sharing');
+    query.setQuery('SELECT A, B, C, D, E, F label A "Name", B "Decision Trees/Random Forests", C "Support Vector Machines", D "Neural Networks" E "Convolutional Neural Networks", F "Total"');
     query.send(handleQueryResponse);
 }
 
